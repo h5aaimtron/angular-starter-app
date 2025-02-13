dd45b1ed2bf460a271658c8e11e6963fcb7a7f56
Scott Ladd <sladd.33@gmail.com>
2025-02-13 15:00:10 -0800

    added base components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
+import { PeopleComponent } from './views/people/people.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'people', component: PeopleComponent }
+];
